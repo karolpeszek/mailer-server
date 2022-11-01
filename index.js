e85e427bfc8969d706d1ca11d7fe2f96eaf3ba91
karolpeszek <karol.peszek@gmail.com>
2022-11-01 15:49:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const formidable = require('formidable');
+const util = require('util');
+const nodemailer = require("nodemailer")
+
+const port = 2137;
+
+
+//app.use(express.urlencoded());
+
+var server = http.createServer(app).listen(port, function () {
+    console.log("Express server listening on port " + port);
+});
+
+app.post('/', function (req, res) {
+    var form = new formidable.IncomingForm(),
+        files = [],
+        fields = new Map();
+
+
+    form
+        .on('field', function (field, value) {
+            fields.set(field, value);
+        })
+        .on('file', function (field, file) {
+            files.push(file);
+        })
+        .on('end', async function () {
+            let transporter;
+            if (fields.get('options') == 'default')
+                transporter = nodemailer.createTransport({
+                    host: "in-v3.mailjet.com",
+                    port: 587,
+                    secure: false,
+                    auth: require('./smtp-cred.json')
+
+
+                });
+            else
+                transporter = nodemailer.createTransport({
+                    host: fields.get('smtpAddress'),
+                    port: fields.get('smtpPort'),
+                    secure: false,
+                    auth: {
+                        user: fields.get('smtpUser'),
+                        pass: fields.get('smtpPass')
+                    }
+                });
+            let message = {
+                from: fields.get('options') == 'default' ? "mailer@karol.gay" : fields.get('from'),
+                to: fields.get('to'),
+                subject: fields.get('subject'),
+                text: fields.get('text'),
+                attachments: []
+            };
+            files.forEach(element => {
+                if (element.originalFilename != '')
+                    message.attachments.push({
+                        filename: element.originalFilename,
+                        path: element.filepath
+                    });
+            });
+
+            console.log(message);
+
+            
+            try {
+                let info = await transporter.sendMail(message);
+                res.redirect('/sent.html?error=0');
+            } catch (ex) {
+                res.redirect('/sent.html?error=1&details=' + encodeURIComponent(ex));
+            }
+
+
+        });
+    files = [];
+    form.parse(req);
+
+});
+
+//app.listen(2137);
